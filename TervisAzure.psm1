775dfa89d3e8081a48fab6a2de8bcc32f360d797
Greg <gkjono@users.noreply.github.com>
2017-09-25 11:20:50 -0400

    + Install-TervisAzure

---
+++ TervisAzure.psm1
@@ -0,0 +1,10 @@
+﻿function Install-TervisAzure {
+    param(
+        [System.Management.Automation.PSCredential]$AzureCredential = $(get-credential -message "Please supply the credentials to access Azure. Username must be in the form UserName@Domain.com")
+    )
+    if (-NOT (Get-Module AzureRM)) {
+        Install-Module AzureRM
+    }
+    $AzureCredential | Export-Clixml $env:USERPROFILE\AzureCredential.txt    
+}
+
