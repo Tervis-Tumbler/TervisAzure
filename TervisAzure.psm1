b0ea93d9acdfee6bd97372afe678bdef0423035b
Greg <gkjono@users.noreply.github.com>
2017-09-25 11:21:41 -0400

    + Connect-ToAzure

--- TervisAzure.psm1
+++ TervisAzure.psm1
@@ -8,3 +8,17 @@
     $AzureCredential | Export-Clixml $env:USERPROFILE\AzureCredential.txt    
 }
 
+function Connect-ToAzure {
+    param(
+        [ValidateSet("Delta","Epsilon","Production/Infrastructure")][String]$Subscription
+    )
+    Import-Module AzureRM
+    $AzureCredential = Import-Clixml $env:USERPROFILE\AzureCredential.txt
+    if ($Subscription -eq "Delta") {
+        Login-AzureRmAccount -Credential $AzureCredential -SubscriptionId "fccc35a6-20b8-4c85-9fe7-94e7f5fd7c37"
+    } elseif ($Subscription -eq "Epsilon") {
+        Login-AzureRmAccount -Credential $AzureCredential -SubscriptionId "58e88142-ab4b-42aa-9c31-5e68f0a5390f"
+    } elseif ($Subscription -eq "Production/Infrastructure") {
+        Login-AzureRmAccount -Credential $AzureCredential -SubscriptionId "8b3835b7-ddd8-41fc-9ee1-297bfe67e2a3"
+    }
+}
